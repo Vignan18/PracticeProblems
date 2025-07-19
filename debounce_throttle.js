546febd0ad7af6a8fe68f579c17006e4c5442a82
Vignan <35171316+Vignan18@users.noreply.github.com>
2025-07-19 18:35:10 +0530

    Create debounce_throttle.js

---
+++ debounce_throttle.js
@@ -0,0 +1,24 @@
+function debounce(fn, delay) {
+  let timeoutId;
+
+  return function (...args) {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      fn.apply(this, args); 
+    }, delay);
+  };
+}
+
+
+function throttle(fn, delay) {
+  let lastCall = 0;
+
+  return function (...args) {
+    const now = Date.now();
+
+    if (now - lastCall >= delay) {
+      lastCall = now;
+      fn.apply(this, args);
+    }
+  };
+}
